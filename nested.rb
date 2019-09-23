7a7dfa31297887f5e034632c5ac9a27a29f4188a
Bailey Wood <woodbmae@gmail.com>
2019-09-23 17:16:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,22 +41,14 @@ end
      }
 end
 
+ describe "dennis_ritchies_language" do
+    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do
+      expect(dennis_ritchies_language).to eq("C")
+    end
+  end
+
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
