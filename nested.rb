77ee6e998a611cc46da98296600e92382d75d568
Bailey Wood <woodbmae@gmail.com>
2019-09-23 17:17:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,24 +39,6 @@ def alan_kay_is_known_for
 	programmer_hash[:alan_kay][:known_for]
 end
 	
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-end
-
- 
 
 def dennis_ritchies_language
   programmer_hash[:dennis_ritchie][:languages]
@@ -76,7 +58,6 @@ def changing_alan
   puts programmer_hash
 end
      
-
 
 def adding_to_dennis
   programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
