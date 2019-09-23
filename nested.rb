a84c563a2c32fca313f4f3cc7894ea93b2066b06
Bailey Wood <woodbmae@gmail.com>
2019-09-23 17:15:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,29 +93,7 @@ end
 
 
 def adding_to_dennis
-  programmer_hash[:dennis_ritchie]
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-end
-
- describe "#adding_to_dennis" do
-    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do
-      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
-    end
-  end
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  puts programmer_hash
 end
 
